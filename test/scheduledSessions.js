b8d977f10a1b5a58ea2fb1955bd7b930b0a75f41
Tomas Duarte <tomasrsduarte@gmail.com>
2018-10-11 18:16:10 +0100

    tests...

---
+++ test/scheduledSessions.js
@@ -0,0 +1,10 @@
+process.env.NODE_ENV = 'test';
+
+describe("Scheduled Sessions", () => {
+
+    describe("/POST Confirm", () => {
+        it('it should return the scheduled session id', (done) => {
+            done();
+        });
+    })
+})
